70fe8779e4a1a4349f9645b106d1aecf76fda4bf
spaghettic0der <max.wittig95@gmail.com>
2016-06-04 21:52:22 +0200

    first working version

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from website_parser import WebsiteParser
 from parser_properties import ParserProperties
 from website_generator import WebsiteGenerator
 
+"""extracts tags from website and generates new website
+from these tags"""
+
 
 def main():
     properties = ParserProperties()
-    properties.source_url = "http://www.golem.de/"
-    properties.tags_to_download = ["img", "h2"]
+    properties.source_url = "https://news.ycombinator.com/news"
+    properties.tags_to_download = ["a"]
     parser = WebsiteParser(properties)
     generator = WebsiteGenerator()
     generator.generate(parser.html_tags_dict)
